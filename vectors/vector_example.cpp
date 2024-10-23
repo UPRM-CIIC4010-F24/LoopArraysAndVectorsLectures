6f1c3b8f39a67b6be6ba539eaa2cbe2234bdcf14
gretchypr <gretchen.bonilla@upr.edu>
2024-10-23 01:03:54 -0400

    Added inheritance code

--- vectors/vector_example.cpp
+++ vectors/vector_example.cpp
@@ -8,7 +8,15 @@ int main() {
     // Adding elements to the vector
     numbers.push_back(40);
     numbers.push_back(50);
+    numbers.push_back(60);
+    numbers.insert(numbers.begin() + 3, 120);
+    numbers.erase(numbers.begin() + 1);
 
+    for(int val: numbers) {
+        cout << val << " ";
+
+    }
+    cout << endl;
     cout << "Vector size: " << numbers.size() << endl;
 
     return 0;
